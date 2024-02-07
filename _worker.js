5c922670a88628a997109d2e3557594fbd158c57
MambaOiOi <158446599+MambaOiOi@users.noreply.github.com>
2024-02-07 19:50:00 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'eedad4df-565e-49d8-96af-db10e5fb2bf3';
 
-const proxyIPs = ['35.219.50.99'];
+const proxyIPs = ['172.232.235.85'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
